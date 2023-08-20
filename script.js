52e5c63695fdc8823b586df831e70a834beb60ea
devransom-github <sundaystanley56@gmail.com>
2023-08-20 01:01:24 +0100

    added code for js

---
+++ script.js
@@ -0,0 +1,13 @@
+// document.querySelector('.mycontainer').addEventListener('click', function(event){
+//     const container = event.target; 
+//     container.classList.toggle('change');
+// })
+
+const Btn = document.querySelector('.btn');
+
+Btn.addEventListener('click', function(event){
+    const container = event.target;
+    container.classList.toggle('change');
+})
+
+
